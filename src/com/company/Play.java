a73a5eac26992ebad86313111c7d9dec4c3b1093
Ameliaaaa321 <103305561+Ameliaaaa321@users.noreply.github.com>
2022-05-02 22:26:42 +0800

    更新5.2.4

--- src/com/company/Play.java
+++ src/com/company/Play.java
@@ -1,26 +1,17 @@
 package com.company;
-
-
 import java.util.ArrayList;
 
 // 还没写王车易位，和棋判定没写三次重复和50次规则
 
 public class Play {
-//    public static void main(String[] args) {
-//        Board board = new Board();
-//        StoreBoard storeBoard = new StoreBoard(board);
-//        initialize(board, 1, 1, 2);
-//        initialize(board, 0, 8, 7);
-//        store(storeBoard, board);
-//    }
-
-    public static void initiateChessboard() {
+    public static void main(String[] args) {
         Board board = new Board();
         StoreBoard storeBoard = new StoreBoard(board);
-        initialize(board, 1, 1, 2);
-        initialize(board, 0, 8, 7);
+        initialize(board, 1, 1, 2);    // 白方棋子初始化
+        initialize(board, 0, 8, 7);    // 黑方棋子初始化
         store(storeBoard, board);
     }
+
     static void store(StoreBoard storeBoard, Board board) {
         storeBoard.z++;
         for (int i = 1; i <= 8; i ++) {
@@ -70,29 +61,42 @@ public class Play {
         }
     }
 
+    // Board就用main方法里创建的那个board就行，整局游戏一个Board
     // 初始时在棋盘的默认位置上创建棋子, row1是王所在行，row2是兵所在行
-    static void initialize(Board board, int side, int row1, int row2) {
+    // 整数side储存黑方or白方，0代表黑方，1代表白方
+    // 返回长度为16的Piece数组，储存某一方的棋子；索引0~7对应从左到右的车马象后王象马车，索引8~15对应从左到右的八个兵
+    static Piece[] initialize(Board board, int side, int row1, int row2) {
+        Piece[] pieces = new Piece[16];
 
         Piece r1 = new R(1, row1, side, board);
         board.positions[1][row1].piece = r1;
+        pieces[0] = r1;
         Piece n1 = new N(2, row1, side, board);
         board.positions[2][row1].piece = n1;
+        pieces[1] = n1;
         Piece b1 = new B(3, row1, side, board);
         board.positions[3][row1].piece = b1;
+        pieces[2] = b1;
         Piece q = new Q(4, row1, side, board);
         board.positions[4][row1].piece = q;
+        pieces[3] = q;
         Piece k = new K(5, row1, side, board);
         board.positions[5][row1].piece = k;
+        pieces[4] = k;
         Piece b2 = new B(6, row1, side, board);
         board.positions[6][row1].piece = b2;
+        pieces[5] = b2;
         Piece n2 = new N(7, row1, side, board);
         board.positions[7][row1].piece = n2;
+        pieces[6] = n2;
         Piece r2 = new R(8, row1, side, board);
         board.positions[8][row1].piece = r2;
+        pieces[7] = r2;
 
         for (int x = 1; x <= 8; x++) {
             Piece p = new P(x, row2, 1, board);
             board.positions[x][row2].piece = p;
+            pieces[x+7] = p;
         }
 
         if (side == 0) {
@@ -100,6 +104,8 @@ public class Play {
         }else if (side == 1) {
             board.k1 = k;
         }
+
+        return pieces;
     }
 
     // 若当前棋格中有子，返回该棋子可走的位置，否则返回null
