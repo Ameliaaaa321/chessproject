1b4d91798145cf96fef5e546f50543b54224412e
Ameliaaaa321 <103305561+Ameliaaaa321@users.noreply.github.com>
2022-05-19 22:15:51 +0800

    更新5.18.9

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -3,8 +3,9 @@ package com.company;
     // 这里stored用的是固定长度的数组。。。
 
 
+import java.util.ArrayList;
 
-    // 储存棋盘上的所有格子
+// 储存棋盘上的所有格子
     public class Board {
         Position[][] positions = new Position[9][9];
         Piece k0, k1;    // 双方的王，便于之后判断将军
@@ -21,8 +22,8 @@ package com.company;
 
     class StoreBoard {
         Board board;
-        int[][][] stored = new int[9][9][31415];    // [坐标x][坐标y][每一次储存]，储存值为0~11，其中0~5依次代表黑方棋子种类，6~11数字代表白方
-        int z = 0;    // z轴，代表每一次储存
+        int currentPlayer;
+        ArrayList<Piece[][]> stored = new ArrayList<>();
 
         public StoreBoard(Board board) {
             this.board = board;
